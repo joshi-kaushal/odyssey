3a78fcc69199162cf2f9ed75abf023ef13e2077e
Kaushal Joshi <7joshikaushal@gmail.com>
2024-02-29 10:50:00 +0530

    chore: zod and cloudinary setup

---
+++ src/lib/cloudinary/upload-image.ts
@@ -0,0 +1,18 @@
+export default async function uploadImage(file: any) {
+
+  const thumbnail = new FormData();
+
+  thumbnail.append("file", file);
+  thumbnail.append("upload_preset", "portfolio");
+
+  const response = await fetch(
+    "https://api.cloudinary.com/v1_1/kaushaljoshi/image/upload",
+    {
+      method: "POST",
+      body: thumbnail,
+    }
+  ).then((response) => response.json())
+    .catch(error => console.log(error));
+
+  return response.url
+}
---
+++ src/lib/zod/BlogSchema.ts
@@ -0,0 +1,23 @@
+import { z } from 'zod';
+
+export const BlogSchema = z.object({
+  id: z.string().optional(),
+  title: z.string().min(1, 'Title should not be null'),
+  slug: z.string().refine((str) => !str.includes(' '), {
+    message: 'Slug must contain only one word (no spaces, dash and underscore allowed)',
+  }),
+  description: z.string().max(200, 'Description should not exceed 200 characters'),
+  url: z
+    .string()
+    .refine((value) => /^(?:https?:\/\/)?(?:www\.)?([a-zA-Z0-9]{2,}\.)+[a-zA-Z]{2,}(?:\/[a-zA-Z0-9\-\._~%\+]*)*$/.test(value), {
+      message: 'Please enter a valid URL',
+    }),
+  date: z.coerce.date(),
+  platform: z.string(),
+  language: z.string(),
+  category: z.string(),
+  tags: z.array(z.string()).optional(),
+  thumbnail: z.string().optional(),
+});
+
+export type BlogFields = z.infer<typeof BlogSchema>
