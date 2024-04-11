88fc3c050533da9d245115a51c849d5edbde2f8f
Kaushal Joshi <7joshikaushal@gmail.com>
2024-04-11 11:02:46 +0530

    feat: prisma functions for tags, category and blog

--- src/lib/prisma/blog.ts
+++ src/lib/prisma/blog.ts
@@ -1,6 +1,8 @@
 "use server";
+import { findCategoryById } from "./category";
 import prisma from "./prisma";
 import { formatISO, parseISO } from "date-fns";
+import { findTagsById } from "./tags";
 
 export async function addNewBlogToDB(data: any) {
   try {
@@ -59,6 +61,36 @@ export async function addNewBlogToDB(data: any) {
   }
 }
 
+export async function getBlogBySlug(slug: string) {
+  try {
+    const blog = await prisma.blog.findFirst({
+      where: { slug: slug },
+    });
+
+    if (blog) {
+      const category = await findCategoryById(blog.categoryId);
+      const tags = await findTagsById(blog.tagId);
+      return {
+        success: true,
+        errors: undefined,
+        data: { ...blog, category, tags },
+      };
+    } else {
+      return {
+        success: false,
+        errors: "No blog matches the provided slug",
+        data: undefined,
+      };
+    }
+  } catch (error) {
+    return {
+      success: false,
+      errors: error,
+      data: undefined,
+    };
+  }
+}
+
 export async function fetchAllBlogs() {
   try {
     const blogs = await prisma.blog.findMany({
@@ -75,6 +107,30 @@ export async function fetchAllBlogs() {
       success: false,
       errors: error,
       data: undefined,
+    };
+  }
+}
+
+export async function deleteBlogByID(id: string) {
+  try {
+    const deleted = await prisma.blog.delete({
+      where: {
+        id: id,
+      },
+    });
+
+    if (deleted.id === id) {
+      return {
+        success: true,
+        errors: undefined,
+        data: "Blog deleted successfully!",
+      };
+    }
+  } catch (error) {
+    return {
+      success: false,
+      errors: `Something went wrong while deleting blog with ID ${id}`,
+      data: "Failed to delete blog. Mostly it's already deleted.",
     };
   }
 }
--- src/lib/prisma/tags.ts
+++ src/lib/prisma/tags.ts
@@ -1,27 +1,40 @@
-"use server"
+"use server";
 
+import { Tags } from "@/types/tags";
 import prisma from "./prisma";
-import kebabCase from "lodash.kebabcase"
+import kebabCase from "lodash.kebabcase";
 
 export async function addTag(label: string) {
-
-    const value = kebabCase(label)
-    return prisma.tags.create({
-        data: {
-            value,
-            label
-        }
-    })
+  const value = kebabCase(label);
+  return prisma.tags.create({
+    data: {
+      value,
+      label,
+    },
+  });
 }
 
 export async function findTag(value: string) {
-    return prisma.tags.findFirst({
-        where: {
-            value
-        }
-    })
+  return prisma.tags.findFirst({
+    where: {
+      value,
+    },
+  });
 }
 
 export async function fetchTags() {
-    return prisma.tags.findMany({})
+  return prisma.tags.findMany({});
+}
+
+export async function findTagsById(tags: string[]) {
+  const response: (Tags | null)[] = await Promise.all(
+    tags.map(async (tagID) => {
+      return await prisma.tags.findFirst({
+        where: {
+          id: tagID,
+        },
+      });
+    })
+  );
+  return response;
 }
